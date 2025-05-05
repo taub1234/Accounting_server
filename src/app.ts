f66a1f4f2f94a2c72f472c6f867433f45781bb5b
Chedvy <Chedvy@gmail.com>
2025-05-05 11:57:00 +0300

    create app and server

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 import express, {Request, Response} from 'express'
+import cors from 'cors'
+
+import {config } from 'dotenv'
+config()
+
+
 
 export const app = express()
 
-app.get('/', (req:Request, res:Response)=>{
+app.use(cors())
+
+
+app.get('/', (req:Request, res:Response, next)=>{
     res.status(200).send('hello')
+    next()
 })
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,14 @@
 import http from 'http'
 import {app} from './app'
 
+import { config } from 'dotenv'
+config()
+
+
+const {HOST= '127.0.0.1', PORT:number= 3900, MONGO_URL= "mongodb://127.0.0.1:27017"}=process.env
+
 
 const server = http.createServer(app)
-server.listen(3001, '127.0.0.1', ()=>{
-    console.log('server')
+server.listen(3900, HOST, ()=>{
+    console.log(`http://${HOST}:3900`);
 })
